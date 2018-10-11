288671e1fe00d87567e8b36cae2753d2847dc02b
Nikhil Razdan <39114568+nsrazdan@users.noreply.github.com>
2018-10-11 13:44:24 -0700

    Update static_array_vector/static_array_vector.cpp

--- static_array_vector/static_array_vector.cpp
+++ static_array_vector/static_array_vector.cpp
@@ -1,10 +1,17 @@
+//created by nsrazdan
+
+template <typename T>
 namespace Snazzy {
     class static_array_vector {
      public:
         static_array_vector() {}
+     private:
+        T[1000] vec;
     };
 }
 
 int main() {
+    
+    Snazzy::static_array_vector<int> test;
     return 0;
 }
