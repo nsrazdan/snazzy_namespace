88333ce294eba694cae42c0f16239a925106d6f7
Nikhil Razdan <39114568+nsrazdan@users.noreply.github.com>
2018-10-27 10:35:03 -0700

    Update singly_linked_list/singly_linked_list.h

--- singly_linked_list/singly_linked_list.h
+++ singly_linked_list/singly_linked_list.h
@@ -7,37 +7,46 @@
 #include <iostream>
 
 namespace Snazzy {
-    template <typename T>
-    class singly_linked_list {
-        struct Node {
-            T item;
-            std::unique_ptr<Node> next;
-        };
-     public: 
-        singly_linked_list();
-        singly_linked_list(T first_item);
-        void print_list();
-        //void insert();
+    
+    template <typename U>
+    class Node {
+     public:
+        Node();
+        Node(const U& item);
      private:
-        std::unique_ptr<Node> head;
+        U item;
+        std::unique_ptr<Node<U>> next;
     };
 
+    template <typename T>
+    class singly_linked_list {
+     public: 
+        singly_linked_list();
+        template <typename U>
+        singly_linked_list(const U& first_item);
+     private:
+        template <typename U>
+        std::unique_ptr<Node<U>> head = nullptr;
+        unsigned int size = 0;
+    };
+
+    template <typename U>
+    Node<U>::Node() {}
+    
+    template <typename U>
+    Node<U>::Node(const U& item) : item(item) {}
+    
     template <typename T>
     singly_linked_list<T>::singly_linked_list() {}
 
     template <typename T>
-    singly_linked_list<T>::singly_linked_list(T first_item) {
-        head = std::unique_ptr<T>(first_item);
+    template <typename U>
+    singly_linked_list<T>::singly_linked_list(const U& first_item) {
+        std::unique_ptr<Node<U>> n(new Node<U>(first_item));
+        head = std::make_unique<Node<U>>(std::move(n));
+        size++;
     }
     
-    template <typename T>
-    void singly_linked_list<T>::print_list() {
-        auto n = head.get();
-        while (n != nullptr) {
-            std::cout << n->item << std::endl;
-            n = (n->next).get();
-        }
-    }
 }
 
 #endif // SINGLY_LINKED_LIST_H__
